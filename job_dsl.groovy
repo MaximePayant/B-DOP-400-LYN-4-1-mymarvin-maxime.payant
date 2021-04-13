be3eeb1e10b8638244c2b40b7f587c4d0563c6ba
Guillaume Soisson <guillaume.soisson@epitech.eu>
2021-04-13 11:20:43 +0200

    ➕ Add SEED job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,6 +1,7 @@
 folder('/Tools') {
     description("Folder for miscellaneous tools.")
 }
+
 job('/Tools/clone-repository') {
     parameters {
         stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
@@ -14,3 +15,37 @@ job('/Tools/clone-repository') {
         }
     }
 }
+
+job('/Tools/SEED') {
+    parameters {
+        stringParam("GITHUB_NAME", "", 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam("DISPLAY_NAME", "", "Display name for the job")
+    }
+
+}
+
+steps {
+        sdl {
+            text {
+                job('$DISPLAY_NAME') {
+                    scm {
+                        git('$GITHUB_NAME')
+                    }
+                    trigger {
+                        scm('@minutes')
+                    }
+                    wrappers {
+                        preBuildCleanup {
+                            deleteDirectories()
+                        }
+                    }
+                    steps {
+                        shell("make fclean")
+                        shell("make")
+                        shell("make tests_run")
+                        shell("make clean")
+                    }
+                }
+            }
+        }
+    }
