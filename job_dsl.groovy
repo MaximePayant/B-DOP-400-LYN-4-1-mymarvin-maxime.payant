94cc94a2da5f9dc51dc110b59fd22ca56df53f9e
Maxime Payant <maxime.payant@epitech.eu>
2021-04-13 12:55:26 +0200

    add job seed (not functional)

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -14,3 +14,17 @@ job('/Tools/clone-repository') {
         }
     }
 }
+job('/Tools/SEED') {
+    parameters {
+        stringParam("GITHUB_NAME", "", "GitHub repository owner/repo_name (e.g.: \"EpitechIT31000/chocolatine\")")
+        stringParam("DISPLAY_NAME", "", "Display name for the job")
+    }
+    steps {
+        dsl {}
+    }
+    wrappers {
+        preBuildCleanup {
+            deleteDirectories()
+        }
+    }
+}
